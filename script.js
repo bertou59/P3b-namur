bafaf6b13b7dbf3bf2e9e0eba88ca98bd86bc871
bertou59 <80994099+bertou59@users.noreply.github.com>
2026-09-01 23:18:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const equipes = [
-    "Leuze A", "Naninne B", "Wépion B", "Rhisnes B", "Aische B", "Sauvenière A", 
-    "St-Germain", "Temploux", "Emines B", "Petit-Waret A", "Mazy A", "Grand-Leez B", 
-    "Boninne A", "Ligny B", "FCO Namur", "Loyers B"
+    "Leuze A", "Naninne B", "CS Wépion B", "Rhisnes B", "Aische B", "RWA Sauvenière", 
+    "St-Germain", "Temploux", "Emines B", "Pt-Waret A", "Mazy", "Gd-Leez B", 
+    "Boninne A", "Ligny B", "FCO Namur", "RUS Loyers B"
 ];
 
+// Le vrai calendrier officiel de la DH (Journées 1 et 2 validées avec vos photos)
 const calendrierFixe = {
-    1: [{dom:"Leuze A",ext:"Loyers B"},{dom:"Naninne B",ext:"FCO Namur"},{dom:"Wépion B",ext:"Ligny B"},{dom:"Rhisnes B",ext:"Boninne A"},{dom:"Aische B",ext:"Grand-Leez B"},{dom:"Sauvenière A",ext:"Mazy A"},{dom:"St-Germain",ext:"Petit-Waret A"},{dom:"Temploux",ext:"Emines B"}],
-    2: [{dom:"Emines B",ext:"Leuze A"},{dom:"Petit-Waret A",ext:"Temploux"},{dom:"Mazy A",ext:"St-Germain"},{dom:"Grand-Leez B",ext:"Sauvenière A"},{dom:"Boninne A",ext:"Aische B"},{dom:"Ligny B",ext:"Rhisnes B"},{dom:"FCO Namur",ext:"Wépion B"},{dom:"Loyers B",ext:"Naninne B"}],
-    3: [{dom:"Leuze A",ext:"Naninne B"},{dom:"Wépion B",ext:"Loyers B"},{dom:"Rhisnes B",ext:"FCO Namur"},{dom:"Aische B",ext:"Ligny B"},{dom:"Sauvenière A",ext:"Boninne A"},{dom:"St-Germain",ext:"Grand-Leez B"},{dom:"Temploux",ext:"Mazy A"},{dom:"Emines B",ext:"Petit-Waret A"}],
-    4: [{dom:"Petit-Waret A",ext:"Leuze A"},{dom:"Mazy A",ext:"Emines B"},{dom:"Grand-Leez B",ext:"Temploux"},{dom:"Boninne A",ext:"St-Germain"},{dom:"Ligny B",ext:"Sauvenière A"},{dom:"FCO Namur",ext:"Aische B"},{dom:"Loyers B",ext:"Rhisnes B"},{dom:"Naninne B",ext:"Wépion B"}],
-    5: [{dom:"Leuze A",ext:"Wépion B"},{dom:"Rhisnes B",ext:"Naninne B"},{dom:"Aische B",ext:"Loyers B"},{dom:"Sauvenière A",ext:"FCO Namur"},{dom:"St-Germain",ext:"Ligny B"},{dom:"Temploux",ext:"Boninne A"},{dom:"Emines B",ext:"Grand-Leez B"},{dom:"Petit-Waret A",ext:"Mazy A"}],
-    6: [{dom:"Mazy A",ext:"Leuze A"},{dom:"Grand-Leez B",ext:"Petit-Waret A"},{dom:"Boninne A",ext:"Emines B"},{dom:"Ligny B",ext:"Temploux"},{dom:"FCO Namur",ext:"St-Germain"},{dom:"Loyers B",ext:"Sauvenière A"},{dom:"Naninne B",ext:"Aische B"},{dom:"Wépion B",ext:"Rhisnes B"}],
-    7: [{dom:"Leuze A",ext:"Rhisnes B"},{dom:"Aische B",ext:"Wépion B"},{dom:"Sauvenière A",ext:"Naninne B"},{dom:"St-Germain",ext:"Loyers B"},{dom:"Temploux",ext:"FCO Namur"},{dom:"Emines B",ext:"Ligny B"},{dom:"Petit-Waret A",ext:"Boninne A"},{dom:"Mazy A",ext:"Grand-Leez B"}],
-    8: [{dom:"Grand-Leez B",ext:"Leuze A"},{dom:"Boninne A",ext:"Mazy A"},{dom:"Ligny B",ext:"Petit-Waret A"},{dom:"FCO Namur",ext:"Emines B"},{dom:"Loyers B",ext:"Temploux"},{dom:"Naninne B",ext:"St-Germain"},{dom:"Wépion B",ext:"Sauvenière A"},{dom:"Rhisnes B",ext:"Aische B"}],
-    9: [{dom:"Leuze A",ext:"Aische B"},{dom:"Sauvenière A",ext:"Rhisnes B"},{dom:"St-Germain",ext:"Wépion B"},{dom:"Temploux",ext:"Naninne B"},{dom:"Emines B",ext:"Loyers B"},{dom:"Petit-Waret A",ext:"FCO Namur"},{dom:"Mazy A",ext:"Ligny B"},{dom:"Grand-Leez B",ext:"Boninne A"}],
-    10: [{dom:"Boninne A",ext:"Leuze A"},{dom:"Ligny B",ext:"Grand-Leez B"},{dom:"FCO Namur",ext:"Mazy A"},{dom:"Loyers B",ext:"Petit-Waret A"},{dom:"Naninne B",ext:"Emines B"},{dom:"Wépion B",ext:"Temploux"},{dom:"Rhisnes B",ext:"St-Germain"},{dom:"Aische B",ext:"Sauvenière A"}],
-    11: [{dom:"Leuze A",ext:"Sauvenière A"},{dom:"St-Germain",ext:"Aische B"},{dom:"Temploux",ext:"Rhisnes B"},{dom:"Emines B",ext:"Wépion B"},{dom:"Petit-Waret A",ext:"Naninne B"},{dom:"Mazy A",ext:"Loyers B"},{dom:"Grand-Leez B",ext:"FCO Namur"},{dom:"Boninne A",ext:"Ligny B"}],
-    12: [{dom:"Ligny B",ext:"Leuze A"},{dom:"FCO Namur",ext:"Boninne A"},{dom:"Loyers B",ext:"Grand-Leez B"},{dom:"Naninne B",ext:"Mazy A"},{dom:"Wépion B",ext:"Petit-Waret A"},{dom:"Rhisnes B",ext:"Emines B"},{dom:"Aische B",ext:"Temploux"},{dom:"Sauvenière A",ext:"St-Germain"}],
-    13: [{dom:"Leuze A",ext:"St-Germain"},{dom:"Temploux",ext:"Sauvenière A"},{dom:"Emines B",ext:"Aische B"},{dom:"Petit-Waret A",ext:"Rhisnes B"},{dom:"Mazy A",ext:"Wépion B"},{dom:"Grand-Leez B",ext:"Naninne B"},{dom:"Boninne A",ext:"Loyers B"},{dom:"Ligny B",ext:"FCO Namur"}],
-    14: [{dom:"FCO Namur",ext:"Leuze A"},{dom:"Loyers B",ext:"Ligny B"},{dom:"Naninne B",ext:"Boninne A"},{dom:"Wépion B",ext:"Grand-Leez B"},{dom:"Rhisnes B",ext:"Mazy A"},{dom:"Aische B",ext:"Petit-Waret A"},{dom:"Sauvenière A",ext:"Emines B"},{dom:"St-Germain",ext:"Temploux"}],
-    15: [{dom:"Leuze A",ext:"Temploux"},{dom:"Emines B",ext:"St-Germain"},{dom:"Petit-Waret A",ext:"Sauvenière A"},{dom:"Mazy A",ext:"Aische B"},{dom:"Grand-Leez B",ext:"Rhisnes B"},{dom:"Boninne A",ext:"Wépion B"},{dom:"Ligny B",ext:"Naninne B"},{dom:"FCO Namur",ext:"Loyers B"}]
+    1: [
+        { dom: "Leuze A", ext: "Naninne B" },
+        { dom: "CS Wépion B", ext: "Rhisnes B" },
+        { dom: "Aische B", ext: "RWA Sauvenière" },
+        { dom: "St-Germain", ext: "Temploux" },
+        { dom: "Emines B", ext: "Pt-Waret A" },
+        { dom: "Mazy", ext: "Gd-Leez B" },
+        { dom: "Boninne A", ext: "Ligny B" },
+        { dom: "FCO Namur", ext: "RUS Loyers B" }
+    ],
+    2: [
+        { dom: "Rhisnes B", ext: "St-Germain" },
+        { dom: "Naninne B", ext: "Mazy" },
+        { dom: "RWA Sauvenière", ext: "CS Wépion B" },
+        { dom: "RUS Loyers B", ext: "Aische B" },
+        { dom: "Ligny B", ext: "FCO Namur" },
+        { dom: "Gd-Leez B", ext: "Boninne A" },
+        { dom: "Pt-Waret A", ext: "Leuze A" },
+        { dom: "Temploux", ext: "Emines B" }
+    ]
 };
 
 let donneesJournees = {};
@@ -28,10 +34,21 @@ let journeeActuelle = 1;
 let ongletActif = 'general';
 
 function genererCalendrierComplet() {
-    for (let j = 1; j <= 15; j++) {
-        donneesJournees[j] = calendrierFixe[j].map(m => ({ dom: m.dom, ext: m.ext, scoreDom: "", scoreExt: "" }));
-        let jr = j + 15;
-        donneesJournees[jr] = calendrierFixe[j].map(m => ({ dom: m.ext, ext: m.dom, scoreDom: "", scoreExt: "" }));
+    // Initialise les structures pour les 30 journées
+    for (let j = 1; j <= totalJournees; j++) {
+        donneesJournees[j] = [];
+    }
+    
+    // Injecte les vraies journées 1 et 2
+    donneesJournees[1] = calendrierFixe[1].map(m => ({ ...m, scoreDom: "", scoreExt: "" }));
+    donneesJournees[2] = calendrierFixe[2].map(m => ({ ...m, scoreDom: "", scoreExt: "" }));
+
+    // Génère le reste des journées pour éviter les bugs d'affichage en attendant les vrais matchs
+    for (let j = 3; j <= 15; j++) {
+        donneesJournees[j] = calendrierFixe[1].map(m => ({ dom: m.dom, ext: m.ext, scoreDom: "", scoreExt: "" }));
+    }
+    for (let j = 16; j <= 30; j++) {
+        donneesJournees[j] = calendrierFixe[1].map(m => ({ dom: m.ext, ext: m.dom, scoreDom: "", scoreExt: "" }));
     }
 }
 
