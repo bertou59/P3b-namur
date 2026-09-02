b976bf0464beb4f19606e00d0bdf6d01d45d6485
bertou59 <80994099+bertou59@users.noreply.github.com>
2026-09-02 23:40:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,3 +50,58 @@ const C = {
 {"visite":"FCO Namur","visiteur":"Petit-Waret A","score":""}
 ]
 };
+"6":[
+{"visite":"Rhisnes B","visiteur":"Boninne A","score":""},
+{"visite":"Naninne B","visiteur":"Sauvenière A","score":""},
+{"visite":"St-Germain","visiteur":"Mazy A","score":""},
+{"visite":"Emines B","visiteur":"Leuze A","score":""},
+{"visite":"Ligny B","visiteur":"Wépion B","score":""},
+{"visite":"Grand-Leez B","visiteur":"Loyers B","score":""},
+{"visite":"Petit-Waret A","visiteur":"Aische B","score":""},
+{"visite":"Temploux","visiteur":"FCO Namur","score":""}
+],
+"7":[
+{"visite":"Wépion B","visiteur":"Leuze A","score":""},
+{"visite":"Aische B","visiteur":"Temploux","score":""},
+{"visite":"Sauvenière A","visiteur":"Petit-Waret A","score":""},
+{"visite":"Loyers B","visiteur":"Naninne B","score":""},
+{"visite":"Ligny B","visiteur":"Grand-Leez B","score":""},
+{"visite":"Mazy A","visiteur":"Emines B","score":""},
+{"visite":"Boninne A","visiteur":"St-Germain","score":""},
+{"visite":"FCO Namur","visiteur":"Rhisnes B","score":""}
+],
+"8":[
+{"visite":"Rhisnes B","visiteur":"Aische B","score":""},
+{"visite":"Leuze A","visiteur":"Mazy A","score":""},
+{"visite":"Naninne B","visiteur":"Ligny B","score":""},
+{"visite":"St-Germain","visiteur":"FCO Namur","score":""},
+{"visite":"Emines B","visiteur":"Boninne A","score":""},
+{"visite":"Grand-Leez B","visiteur":"Wépion B","score":""},
+{"visite":"Petit-Waret A","visiteur":"Loyers B","score":""},
+{"visite":"Temploux","visiteur":"Sauvenière A","score":""}
+],
+"9":[
+{"visite":"Wépion B","visiteur":"Mazy A","score":""},
+{"visite":"Aische B","visiteur":"St-Germain","score":""},
+{"visite":"Sauvenière A","visiteur":"Rhisnes B","score":""},
+{"visite":"Loyers B","visiteur":"Temploux","score":""},
+{"visite":"Ligny B","visiteur":"Petit-Waret A","score":""},
+{"visite":"Grand-Leez B","visiteur":"Naninne B","score":""},
+{"visite":"Boninne A","visiteur":"Leuze A","score":""},
+{"visite":"FCO Namur","visiteur":"Emines B","score":""}
+],
+"10":[
+{"visite":"Rhisnes B","visiteur":"Loyers B","score":""},
+{"visite":"St-Germain","visiteur":"Sauvenière A","score":""},
+{"visite":"Emines B","visiteur":"Aische B","score":""},
+{"visite":"Leuze A","visiteur":"FCO Namur","score":""},
+{"visite":"Mazy A","visiteur":"Boninne A","score":""},
+{"visite":"Naninne B","visiteur":"Wépion B","score":""},
+{"visite":"Petit-Waret A","visiteur":"Grand-Leez B","score":""},
+{"visite":"Temploux","visiteur":"Ligny B","score":""}
+],
+"11":[
+{"visite":"Naninne B","visiteur":"Petit-Waret A","score":""},
+{"visite":"Wépion B","visiteur":"Boninne A","score":""},
+{"visite":"Loyers B","visiteur":"St-Germain","score":""},
+{"visite":"
